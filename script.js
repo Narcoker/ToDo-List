5cd8fff28dc1ea5be16df27bf4593f93cc3c59ac
Narcoker <jykim9868@gmail.com>
2023-01-12 17:42:38 +0900

    ✨ Feat: 새로고침이나 탭을 열고 닫았을 때 데이터 유지 기능 추가 Local Storage에 데이터를 저장하고 불러와서 데이터 유지

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector('#todo-list');
 
-const createTodo = function () {
-    const todoList = document.querySelector('#todo-list');
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+
+const createTodo = function (storageData) {
+    let todoContents = todoInput.value;
+    if (storageData) {
+        todoContents = storageData.contents;
+    }
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     const newBtn = document.createElement('button');
 
     newBtn.addEventListener('click', () => {
         newLi.classList.toggle('complete');
+        saveItemsFn();
     });
 
     newLi.addEventListener('dblclick', () => {
         newLi.remove();
+        saveItemsFn();
     });
 
-    newSpan.textContent = todoInput.value;
+    if (storageData?.complete) {
+        newLi.classList.add('complete');
+    }
+
+    newSpan.textContent = todoContents;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
     console.log(newLi)
     todoInput.value = "";
+    saveItemsFn();
 }
 
 const keyCodeCheck = function () {
@@ -33,5 +46,32 @@ const deleteAll = function () {
     for (let i = 0; i < liList.length; i++) {
         console.log(liList[i])
         liList[i].remove();
+    }
+}
+
+const saveItemsFn = function () {
+    const saveItems = [];
+    for (let i = 0; i < todoList.children.length; i++) {
+        const todoObj = {
+            contents: todoList.children[i].querySelector('span').textContent,
+            complete: todoList.children[i].classList.contains('complete')
+        }
+        saveItems.push(todoObj);
+    }
+
+    if (saveItems.length === 0) {
+        localStorage.removeItem('saved-items');
+        return;
+    }
+
+    if (saveItems.length !== 0) {
+        localStorage.setItem('saved-items', JSON.stringify(saveItems));
+        return;
+    }
+}
+
+if (savedTodoList) {
+    for (let i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
     }
 }
